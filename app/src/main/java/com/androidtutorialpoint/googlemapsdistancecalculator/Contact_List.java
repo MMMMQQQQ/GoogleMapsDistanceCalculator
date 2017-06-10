fbe081989afe3e11bcc0b7ad7a6fc5530ba3c3bb
MMMMQQQQ <qiumingming7@gmail.com>
2017-06-10 17:55:24 +0200

    Initial commit

---
+++ app/src/main/java/com/androidtutorialpoint/googlemapsdistancecalculator/Contact_List.java
@@ -0,0 +1,10 @@
+package com.androidtutorialpoint.googlemapsdistancecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/4 0004.
+ */
+
+public class Contact_List extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/androidtutorialpoint/googlemapsdistancecalculator/Settings.java
@@ -0,0 +1,8 @@
+package com.androidtutorialpoint.googlemapsdistancecalculator;
+
+/**
+ * Created by Administrator on 2017/6/4 0004.
+ */
+
+public class Settings {
+}
